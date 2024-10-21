02310caf7b94739ff40569a7d917ee756fd0740e
Corster8 <caeahaiteah@gmail.com>
2024-10-21 17:41:23 -0400

    removed pageStyle.js

--- public/scripts/pageStyle.js
+++ public/scripts/pageStyle.js
@@ -1,8 +1,8 @@
 
-const pageName = window.location.pathname.split('/').pop().replace('.html', '');
+// const pageName = window.location.pathname.split('/').pop().replace('.html', '');
 
-if (pageName) {
-    document.body.classList.add(`${pageName}-page`);
-} else {
-    document.body.classList.add('index-page'); 
-}
+// if (pageName) {
+//     document.body.classList.add(`${pageName}-page`);
+// } else {
+//     document.body.classList.add('index-page'); 
+// }
