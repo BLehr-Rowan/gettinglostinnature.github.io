faeaf08639c3ec690c78cf3666ad2e2bd0265245
chille53 <chille53@students.rowan.edu>
2024-11-07 21:51:50 -0500

    ehh

--- pages/map.cjs
+++ pages/map.cjs
@@ -5,13 +5,17 @@
 module.exports = {
     name: "/map",
     method: "get",
-    execute(req, res)
-    {
-        if(!req.cookies.username)
-        {
+    execute(req, res) {
+        // Check if the user is logged in based on the presence of the username cookie
+        const loggedIn = !!req.cookies.username;
+
+        // If not logged in, redirect to the home page
+        if (!loggedIn) {
             res.redirect("/");
             return;
         }
-        res.render("map");
+
+        // Render the map page with the loggedIn variable
+        res.render("map", { loggedIn });
     }
 };
